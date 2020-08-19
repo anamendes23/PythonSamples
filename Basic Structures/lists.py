a1f80da0daae62a141f912ec971ea0f5b2162f1c
anamendes23 <anamendes23@gmail.com>
2020-08-18 19:57:56 -0700

    added more samples for lists and tuples

--- Basic Structures/lists.py
+++ Basic Structures/lists.py
@@ -33,4 +33,68 @@ print(fruits)
 fruits[2] = "Strawberry"
 print(fruits)
 
+# Tuples: sequences of elements of any type, that are immutable
+# written between parenthesis
+# the position of the elements inside the tuple have meaning
+fullname = ('Grace', 'M', 'Hopper')
+# used to return multiple values in functions
 
+def test():
+    return "Grace", "M", "Hopper"
+
+result = test()
+type(result)
+print(result)
+
+# you can unpack tuples
+first, middle, last = result
+print(first, middle, last)
+
+first, middle, last = test()
+print(first, middle, last)
+
+# Iterating over Lists and Tuples
+animals = ["Lion", "Zebra", "Dolphin", "Monkey"]
+chars = 0
+
+for animal in animals:
+    chars += len(animals)
+
+print("Total characters: {}, Average length: {}".format(chars, chars/len(animals)))
+
+# enumate function
+winners = ["Ashley", "Dilan", "Reese"]
+for index, person in enumerate(winners):
+    print("{} - {}".format(index + 1, person))
+
+# enumerate returns a tuple with index and element information
+
+# List of tuples containing name and email of each person
+def full_emails(people):
+    result = []
+    for email, name in people:
+        result.append("{} <{}>".format(name, email))
+    return result
+
+print(full_emails([("alex@example.com", "Alex Diego"), ("shay@example,com", "Shay Brandt")]))
+
+# List Comprehensions
+multiples = []
+for x in range(1,11):
+    multiples.append(x*7)
+
+print(multiples)
+
+# there's a better way to do this
+multiples = [ x*7 for x in range(1,11) ]
+print(multiples)
+
+# List comprehensions: let us create new lists based on sequences or ranges
+
+languages = ["Python", "Java", "Ruby", "Perl", "Go", "C"]
+lengths = [len(language) for language in languages]
+print(lengths)
+
+# get all numbers divisible by 3 between 0 and 100
+z = [x for x in range(0,101) if x % 3 == 0]
+print(z)
